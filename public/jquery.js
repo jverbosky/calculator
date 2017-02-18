23cda8d69e3ae481bae86a42c3d5b12d354cfaf3
jverbosk <jverbosk@gmail.com>
2017-02-18 14:06:08 -0500

    added comment to jquery.js (attributed source for accessing textarea contents), cleaned up layout

--- public/jquery.js
+++ public/jquery.js
@@ -3,6 +3,13 @@
 //
 // per StackOverflow question:
 // http://stackoverflow.com/questions/21026041/when-click-a-button-add-value-to-textarea
+// ---------------------------------------------------
+// borrowed textarea text capture script from here:
+// http://jsfiddle.net/acdcjunior/FZABp/17/
+//
+// per StackOverflow question:
+// http://stackoverflow.com/questions/5831413/get-textarea-text-with-javascript-or-jquery
+// ---------------------------------------------------
 
 $(document).ready(function () {
     $(".buttons").click(function () {
