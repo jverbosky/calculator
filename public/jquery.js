44ee1b7b95ee906ad32dd9b566a49066729991f3
jverbosk <jverbosk@gmail.com>
2017-02-18 17:04:34 -0500

    playing with new way to populate textarea, need to stop for now (family time)

--- public/jquery.js
+++ public/jquery.js
@@ -11,19 +11,59 @@
 // http://stackoverflow.com/questions/5831413/get-textarea-text-with-javascript-or-jquery
 // ---------------------------------------------------
 
+// Clicking buttons, showing textarea contents and clearing textarea contents working
+// but unable to add (append) more to textarea after clearing textarea
+// Research indicates need to use val() instead of append()
+// Saving this block while testing with val()
+
+// $(document).ready(function () {
+
+//     $(".buttons").click(function () {
+//         var cntrl = $(this).html();
+//         $("#userInput").append(cntrl);
+//     });
+
+//     $(".clear").click(function () {
+//         $("#userInput").val('');
+//     });
+
+//     $("#btn1").click(function () {
+//         alert(  $("#userInput").val()  );
+//     });
+
+// });
+
+// ---------------------------------------------------
+
+// Getting ideas from this fiddle - reference later for ideas on appending operator to number:
+// http://jsfiddle.net/gYFP3/4/
+
+
 $(document).ready(function () {
+
+    // This new block isn't working but need to stop for now, will play more later...
+
     $(".buttons").click(function () {
-        var cntrl = $(this).html();
-        $("#userInput").append(cntrl);
+        var buttonValue = $(this).html();
+        var $userInput = $(#userInput)
+        var val = $userInput.val();
+        $userInput.val(val + buttonValue);
     });
+
+    //  not working ^^^
+
     $(".clear").click(function () {
-        // var cntrl = $(this).html();
         $("#userInput").val('');
     });
+
     $("#btn1").click(function () {
         alert(  $("#userInput").val()  );
     });
+
 });
+
+
+// ---------------------------------------------------
 
 // playing around here to try and collect user input from text area
 
