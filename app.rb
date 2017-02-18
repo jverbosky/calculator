54c2237f16461517f7d1d31d76fdccd845ee6e22
jverbosk <jverbosk@gmail.com>
2017-02-18 12:06:49 -0500

    added screenshot of calculator app mockup (logic flow)

--- app.rb
+++ app.rb
@@ -3,10 +3,15 @@ require 'sinatra/reloader' if development?  # automatically reload app.rb on sav
 require_relative 'calculator.rb'  # load Ruby script (same directory)
 
 get '/' do  # route to load the Calculator Start page
-  erb :calculator_start
+  erb :start
 end
 
 post '/calculate' do  # route that accesses input from form's post > action
-  num = params[:digit]  # params used to access input from post > action (name="ISBN")
-  erb :calculator_results  # load isbn_status.erb file with ISBN check results output
+  num = params[:input]  # params used to access input from post > action (name="ISBN")
+  erb :calculate  # load isbn_status.erb file with ISBN check results output
+end
+
+post '/results' do
+  num = params[:num_2]
+  erb :results
 end
