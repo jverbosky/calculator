bc12aee594fd7054cdd20a439eedd7f4ff157402
jverbosk <jverbosk@gmail.com>
2017-02-18 12:57:10 -0500

    added comment to jquery.js - attributed source

--- public/jquery.js
+++ public/jquery.js
@@ -1,3 +1,9 @@
+// borrowed button press capture script from here:
+// http://jsfiddle.net/arunpjohny/YD6PL/
+//
+// per StackOverflow question:
+// http://stackoverflow.com/questions/21026041/when-click-a-button-add-value-to-textarea
+
 $(document).ready(function () {
     $(".buttons").click(function () {
         var cntrl = $(this).html();
