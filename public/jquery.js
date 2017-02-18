72e45b3064ab27bcd1984f565f270a8483199fba
jverbosk <jverbosk@gmail.com>
2017-02-18 17:53:06 -0500

    tweaked jquery to use val() instead of append() for populating textarea

--- public/jquery.js
+++ public/jquery.js
@@ -3,6 +3,8 @@
 //
 // per StackOverflow question:
 // http://stackoverflow.com/questions/21026041/when-click-a-button-add-value-to-textarea
+//
+// note that I changed this due to append() not working after clearing via val()
 // ---------------------------------------------------
 // borrowed textarea text capture script from here:
 // http://jsfiddle.net/acdcjunior/FZABp/17/
@@ -10,69 +12,28 @@
 // per StackOverflow question:
 // http://stackoverflow.com/questions/5831413/get-textarea-text-with-javascript-or-jquery
 // ---------------------------------------------------
-
-// Clicking buttons, showing textarea contents and clearing textarea contents working
-// but unable to add (append) more to textarea after clearing textarea
-// Research indicates need to use val() instead of append()
-// Saving this block while testing with val()
-
-// $(document).ready(function () {
-
-//     $(".buttons").click(function () {
-//         var cntrl = $(this).html();
-//         $("#userInput").append(cntrl);
-//     });
-
-//     $(".clear").click(function () {
-//         $("#userInput").val('');
-//     });
-
-//     $("#btn1").click(function () {
-//         alert(  $("#userInput").val()  );
-//     });
-
-// });
-
+// got example of appending numbers from here:
+// http://stackoverflow.com/questions/841722/append-text-to-input-field
 // ---------------------------------------------------
-
-// Getting ideas from this fiddle - reference later for ideas on appending operator to number:
-// http://jsfiddle.net/gYFP3/4/
-
 
 $(document).ready(function () {
 
-    // This new block isn't working but need to stop for now, will play more later...
-
-    $(".buttons").click(function () {
+    // have button values appear/append in textarea field (#userInput)
+    $(".button").click(function () {
         var buttonValue = $(this).html();
-        var $userInput = $(#userInput)
-        var val = $userInput.val();
-        $userInput.val(val + buttonValue);
+        $("#userInput").val($("#userInput").val() + buttonValue);
     });
 
-    //  not working ^^^
-
+    // clear contents of textarea field
     $(".clear").click(function () {
         $("#userInput").val('');
     });
 
+    // show contents of textarea field
+    // groundwork for passing values via post
     $("#btn1").click(function () {
         alert(  $("#userInput").val()  );
     });
 
 });
 
-
-// ---------------------------------------------------
-
-// playing around here to try and collect user input from text area
-
-// $(document).ready(function () {
-//   var user_input = $("#text_area");
-// });
-
-// $(document).ready(function () {
-//     $('#btn1').click(function() {
-//         alert(  $("#userInput").val()  );
-//     });
-// });
