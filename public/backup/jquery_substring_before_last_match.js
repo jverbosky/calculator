47a9c7c708a1e136f8d6287d5bb830fc628c9cda
jverbosk <jverbosk@gmail.com>
2017-02-20 15:51:23 -0500

    having issues with conditional output, broke getLastNum and getAllButLastNum into separate functions to try and solve

---
+++ public/backup/jquery_substring_before_last_match.js
@@ -0,0 +1,70 @@
+$(document).ready(function () {
+
+    // have button values appear/append in textarea field (#userInput)
+    $(".input").click(function () {
+        var buttonValue = $(this).html();
+        $("#userInput").val($("#userInput").val() + buttonValue);
+    });
+
+
+    // need to add function for operator buttons - can currently enter them first without numbers
+    // also need to add logic to prevent multiple concurrent operators
+
+    // add a zero before . if there isn't another number before it
+    $(".dot").click(function () {
+        // var buttonValue = $(this).html();
+        var currentExpression = $("#userInput").val();
+        var dot = ".";
+        // var zeroPad = "0";
+        // var lastChar = currentExpression.substr(currentExpression.length - 1);
+        // // add variable for string after previous operator (want to disallow adding . if lastNum already has)
+        // var lastNum = "something"; // need to work out the "something" part  ^~^;
+        // if (currentExpression.length == 0) {
+        //   $("#userInput").val(zeroPad + dot);
+        // } else if (lastChar == "." ) {
+        //   $("#userInput").val(currentExpression);
+        // // need to test after working out lastNum - not sure if the match target is right here
+        // } else if (lastNum.match( "[.]" ) ) {
+        //   // $("#userInput").val(currentExpression);
+        //   $("#userInput").val("something");
+        // } else if (lastChar.match( "[-+*/]" ) ) {
+        //   $("#userInput").val(currentExpression + zeroPad + dot);
+        // } else {
+          $("#userInput").val(currentExpression + dot);
+        // }
+    });
+
+    // flip the sign (negative/positive) of the current number
+    $(".sign").click(function () {
+        var currentExpression = $("#userInput").val();
+        var lastNum = "";
+        var allButLastNum = "";
+        var matchOp = currentExpression.match( /[+/*-]/g );  // order of operators in regex vital!
+        var lastOpIndex = currentExpression.lastIndexOf( matchOp[matchOp.length-1] );  // index of last operator
+        if (currentExpression.match( "[-+*/]" ) ) {  // if there are any operators
+          lastNum = currentExpression.substr( lastOpIndex+1 );  // save number after last operator
+        } else {
+          lastNum = currentExpression;  // otherwise save lonely number
+        }
+        if (currentExpression.match( "[-+*/]" ) ) {  // if there are any operators
+          allButLastNum = currentExpression.substr(0, lastOpIndex+1);  // save everything before last number
+        }
+
+        // test code for concatenating number (integer/float) after last operator
+        // $("#userInput").val(currentExpression + lastNum);
+
+        // test code for all but the lastNum
+        $("#userInput").val(allButLastNum);
+
+        // Next add logic for concatenating "(-" and ")" onto lastNum if not already negative
+
+        // $("#userInput").val("(" + lastNum + ")");
+    });
+
+    // clear contents of textarea field
+    $(".clear").click(function () {
+        $("#userInput").val('');
+    });
+
+});
+
--- public/jquery.js
+++ public/jquery.js
@@ -2,8 +2,8 @@ $(document).ready(function () {
 
     // have button values appear/append in textarea field (#userInput)
     $(".input").click(function () {
-        var buttonValue = $(this).html();
-        $("#userInput").val($("#userInput").val() + buttonValue);
+      var buttonValue = $(this).html();
+      $("#userInput").val($("#userInput").val() + buttonValue);
     });
 
 
@@ -12,53 +12,88 @@ $(document).ready(function () {
 
     // add a zero before . if there isn't another number before it
     $(".dot").click(function () {
-        // var buttonValue = $(this).html();
-        var currentExpression = $("#userInput").val();
-        var dot = ".";
-        // var zeroPad = "0";
-        // var lastChar = currentExpression.substr(currentExpression.length - 1);
-        // // add variable for string after previous operator (want to disallow adding . if lastNum already has)
-        // var lastNum = "something"; // need to work out the "something" part  ^~^;
-        // if (currentExpression.length == 0) {
-        //   $("#userInput").val(zeroPad + dot);
-        // } else if (lastChar == "." ) {
-        //   $("#userInput").val(currentExpression);
-        // // need to test after working out lastNum - not sure if the match target is right here
-        // } else if (lastNum.match( "[.]" ) ) {
-        //   // $("#userInput").val(currentExpression);
-        //   $("#userInput").val("something");
-        // } else if (lastChar.match( "[-+*/]" ) ) {
-        //   $("#userInput").val(currentExpression + zeroPad + dot);
-        // } else {
-          $("#userInput").val(currentExpression + dot);
-        // }
+      // var buttonValue = $(this).html();
+      var currentExpression = $("#userInput").val();
+      var dot = ".";
+      // var zeroPad = "0";
+      // var lastChar = currentExpression.substr(currentExpression.length - 1);
+      // // add variable for string after previous operator (want to disallow adding . if lastNum already has)
+      // var lastNum = "something"; // need to work out the "something" part  ^~^;
+      // if (currentExpression.length == 0) {
+      //   $("#userInput").val(zeroPad + dot);
+      // } else if (lastChar == "." ) {
+      //   $("#userInput").val(currentExpression);
+      // // need to test after working out lastNum - not sure if the match target is right here
+      // } else if (lastNum.match( "[.]" ) ) {
+      //   // $("#userInput").val(currentExpression);
+      //   $("#userInput").val("something");
+      // } else if (lastChar.match( "[-+*/]" ) ) {
+      //   $("#userInput").val(currentExpression + zeroPad + dot);
+      // } else {
+        $("#userInput").val(currentExpression + dot);
+      // }
     });
+
+    function getLastNum(currentExpression) {
+      var lastNum = "";
+      var matchOp = currentExpression.match( /[+/*-]/g );  // order of operators in regex vital!
+      var lastOpIndex = currentExpression.lastIndexOf( matchOp[matchOp.length-1] );  // index of last operator
+      if (currentExpression.match( "[-+*/]" ) ) {  // if there are any operators
+        lastNum = currentExpression.substr( lastOpIndex+1 );  // save number after last operator
+      } else {
+        lastNum = currentExpression;  // otherwise save lonely number
+      }
+      return lastNum
+    }
+
+    function getAllButLastNum(currentExpression) {
+      var lastNum = getLastNum(currentExpression)
+      var allButLastNum = "";
+      var matchOp = currentExpression.match( /[+/*-]/g );  // order of operators in regex vital!
+      var lastOpIndex = currentExpression.lastIndexOf( matchOp[matchOp.length-1] );  // index of last operator
+      if (currentExpression.match( "[-+*/]" ) ) {  // if there are any operators
+        allButLastNum = currentExpression.substr( 0, lastOpIndex+1 );  // save everything before last number
+      }
+      return allButLastNum
+    }
+
 
     // flip the sign (negative/positive) of the current number
     $(".sign").click(function () {
         var currentExpression = $("#userInput").val();
-        var lastNum = "";
-        var allButLastNum = "";
-        var matchOp = currentExpression.match( /[+/*-]/g );  // order of operators in regex vital!
-        var lastOpIndex = currentExpression.lastIndexOf( matchOp[matchOp.length-1] );  // index of last operator
-        if (currentExpression.match( "[-+*/]" ) ) {  // if there are any operators
-          lastNum = currentExpression.substr( lastOpIndex+1 );  // save number after last operator
-        } else {
-          lastNum = currentExpression;  // otherwise save lonely number
-        }
-        if (currentExpression.match( "[-+*/]" ) ) {  // if there are any operators
-          allButLastNum = currentExpression.substr(0, lastOpIndex+1);  // save everything before last number
-        }
+        var lastNum = getLastNum(currentExpression)
+        var allButLastNum = getAllButLastNum(currentExpression)
+        $("#userInput").val(currentExpression + allButLastNum);
+        // var workingExpression = currentExpression  // copy to address access issues after lastOpIndex
+        // var lastNum = "";
+        // var allButLastNum = "";
+        // var matchOp = workingExpression.match( /[+/*-]/g );  // order of operators in regex vital!
+        // var lastOpIndex = workingExpression.lastIndexOf( matchOp[matchOp.length-1] );  // index of last operator
+        // if (workingExpression.match( "[-+*/]" ) ) {  // if there are any operators
+        //   lastNum = workingExpression.substr( lastOpIndex+1 );  // save number after last operator
+        // } else {
+        //   lastNum = workingExpression;  // otherwise save lonely number
+        // }
+        // if (workingExpression.match( "[-+*/]" ) ) {  // if there are any operators
+        //   allButLastNum = workingExpression.substr( 0, lastOpIndex+1 );  // save everything before last number
+        // }
+
+        // test code for output value of lastNum
+        // $("#userInput").val(lastNum);
 
         // test code for concatenating number (integer/float) after last operator
         // $("#userInput").val(currentExpression + lastNum);
 
         // test code for all but the lastNum
-        $("#userInput").val(allButLastNum);
+        // $("#userInput").val(allButLastNum);
 
         // Next add logic for concatenating "(-" and ")" onto lastNum if not already negative
-
-        // $("#userInput").val("(" + lastNum + ")");
+        // if (currentExpression.match( "[-+*/]" ) ) {
+        //  $("#userInput").val(allButLastNum + "(" + (lastNum * -1) + ")" );
+        // } else if (currentExpression > 0 ) {
+           // $("#userInput").val(currentExpression + currentExpression);
+          // $("#userInput").val($("#userInput").val() * -1);
+        // }
     });
 
     // clear contents of textarea field
