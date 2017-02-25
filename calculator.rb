34f3a53f8b196dc659176f94d62dc138d7254c66
jverbosk <jverbosk@gmail.com>
2017-02-25 16:36:55 -0500

    squashed some bugs, documented in bugs_list.txt

--- calculator.rb
+++ calculator.rb
@@ -33,6 +33,15 @@ def calculate(num_1, operator, num_2)
   return result
 end
 
+# Method to return integer if value after decimal is zero
+def evaluate_result(num)
+  if num == 0
+    return 0
+  else
+    num % num.to_i == 0 ? num.to_i : num
+  end
+end
+
 # Method to compute result of all mathmatical expressions (multiple supported)
 def get_result(string)
   numbers = get_numbers(string)
@@ -42,14 +51,19 @@ def get_result(string)
   operator_index = 0
   num_1 = evaluate_number(numbers[number_index])  # start with first number in numbers array
   result = 0
-  while number_index < operators.length  # calculate pairs of numbers (left to right) until done
-    operator = operators[operator_index]  # start with first operator in operators array
-    num_2 = evaluate_number(numbers[number_index + 1])  # start with second number in numbers array
-    result = calculate(num_1, operator, num_2)  # perform calculation
-    break if result == "Error"  # if expression divides by zero anywhere, return "Error"
-    num_1 = result  # store result in num_1 and use it for next iteration (or return when loop ends)
-    number_index += 1  # increase counter to grab next number for next iteration
-    operator_index += 1  # increase counter to grab next operator for next iteration
+  if operators.length == 0  # if there aren't any operators, return the single number
+    result = num_1
+  else  # otherwise, start calculating...
+    while number_index < operators.length  # calculate pairs of numbers (left to right) until done
+      operator = operators[operator_index]  # start with first operator in operators array
+      num_2 = evaluate_number(numbers[number_index + 1])  # start with second number in numbers array
+      result = calculate(num_1, operator, num_2)  # perform calculation
+      break if result == "Error"  # if expression divides by zero anywhere, return "Error"
+      num_1 = result  # store result in num_1 and use it for next iteration (or return when loop ends)
+      number_index += 1  # increase counter to grab next number for next iteration
+      operator_index += 1  # increase counter to grab next operator for next iteration
+    end
   end
+  result = evaluate_result(result)  # check if number is an integer
   return result
 end
