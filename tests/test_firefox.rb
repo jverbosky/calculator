4377c1e94e8d745071ebe55acc037463d0644d33
jverbosky <jverbosk@gmail.com>
2017-10-27 12:00:07 -0400

    trying selenium image

--- tests/test_firefox.rb
+++ tests/test_firefox.rb
@@ -23,13 +23,13 @@ class ThrivyTestCase < Test::Unit::TestCase
     # @driver = Selenium::WebDriver.for :firefox #, options: caps
 
     caps = Selenium::WebDriver::Firefox::Options.new(args: ['-headless'])
-    @driver = Selenium::WebDriver.for :firefox, options: caps
+    @driver = Selenium::WebDriver.for :firefox #, options: caps
 
     # target_size = Selenium::WebDriver::Dimension.new(768, 894)
     # @driver.manage.window.size = target_size
     # @driver.navigate.to("https://calculator-jv.herokuapp.com/")
     # @wait = Selenium::WebDriver::Wait.new(:timeout => 15)
-  end  
+  end
 
   def teardown
     # @driver.close
